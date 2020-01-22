cc94809e136838c23baf1b5fbc75de3c7dc69569
Ray Lin <raylin009@gmail.com>
2020-01-22 08:37:53 -0800

    deleted resumeBlock.jsx

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -4,13 +4,10 @@ import Skills from './Skills.jsx';
 import Projects from './Projects.jsx';
 import Experience from './Experience.jsx';
 import Education from './Education.jsx';
-import ResumeBlock from './ResumeBlock.jsx';
 const { resumeData } = require('../../server/resumeData.js');
 
 const Resume = () => (
   <div className="right-inner">
-    {/* {console.log(resumeData)} */}
-    {/* <ResumeBlock data={ resumeData }/> */}
 
     <Intro data={resumeData[0]}/>
     <Skills data={resumeData[1]}/>
