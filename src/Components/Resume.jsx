e6973ad177d78b79de3b4580e825b9acd8b8c298
Ray Lin <raylin009@gmail.com>
2020-01-23 08:37:58 -0800

    added resume btn

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -8,12 +8,16 @@ const { resumeData } = require('../../server/resumeData.js');
 
 const Resume = () => (
   <div className="right-inner">
-
     <Intro data={resumeData[0]}/>
     <Skills data={resumeData[1]}/>
     <Education data={resumeData[2]}/>
     <Projects data={resumeData[3]}/>
     <Experience data={resumeData[4]}/>
+    <a className="resume-btn" href="https://raylin009.github.io/bult_cutter/Ray_Lin_Resume.pdf">
+      <div className="resume-link">
+          Grab a pdf of my full resume
+      </div>
+    </a>
   </div>
 )
 
